f4d4f065d42f063f66f053d5dd752b7ba508ad64
umakantyadav11 <umakant.yadav@healthify.co>
2023-06-13 12:29:40 +0530

    added

--- langchain/llms/ai21.py
+++ langchain/llms/ai21.py
@@ -12,13 +12,14 @@ from langchain.utils import get_from_dict_or_env
 class AI21PenaltyData(BaseModel):
     """Parameters for AI21 penalty data."""
 
+
     scale: int = 0
     applyToWhitespaces: bool = True
     applyToPunctuations: bool = True
     applyToNumbers: bool = True
     applyToStopwords: bool = True
     applyToEmojis: bool = True
-    
+
 
 
 class AI21(LLM):
